419baefe80dc4e598cafba3c3333d8f824176322
Max S <slastonm@gmail.com>
2023-02-17 20:17:47 +0200

    Events in vue cw-16

---
+++ 16/vue-hard/hard/src/components/ModalWindow.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="overlay" @click.self="emmitData">
+        <div class="modal">
+            {{title}}
+            <div>
+                {{ info }}
+            </div>
+            <p>
+                {{ propValue }}
+            </p>
+            <p>
+                {{ fetchData }}
+            </p>
+            <button  @click.self="emmitData">
+                Close modal
+            </button>
+        </div>        
+    </div>
+
+</template>
+
+<script>
+export default {
+    name:'ModalWindow',
+    props:['title', 'info', 'propValue'],
+    data(){
+        return{
+            fetchData:''
+        }
+    },
+    methods:{
+        closeModal(){
+            alert(111);
+        },
+        emmitData(){
+            this.$emit('close')
+        }
+    },
+    mounted(){
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => this.fetchData = json)
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .overlay{
+        display: flex;
+        position: fixed;
+        top: 0;
+        z-index: 1;
+        background-color: rgba(0,0,0,0.5);
+        width: 100%;
+        height: 100%;
+    }
+    .modal{
+        width: 300px;
+        background-color: #fff;
+        padding: 50px;
+        margin: auto;
+        border: 2px solid red;
+        &.discount{
+            background-color: rgb(84, 195, 84);
+        }
+    }
+</style>
--- 16/vue-hard/hard/src/views/HomeView.vue
+++ 16/vue-hard/hard/src/views/HomeView.vue
@@ -1,18 +1,49 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <h3>
+      Hello vue
+    </h3>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <button @click="toggleModal">
+      Show modal
+    </button>
+    <div v-if="modalIs">
+      <ModalWindow 
+      info="Component info" 
+      title="Hello props value" 
+      :propValue="true"
+      @close="toggleModal"/>
+    </div>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import ModalWindow from '@/components/ModalWindow.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HelloWorld,
+    ModalWindow
+  },
+  data(){
+    return{
+      modalIs:false
+    }
+  },
+  methods:{
+    toggleModal(){
+      this.modalIs = !this.modalIs
+    }
   }
 }
 </script>
+<style lang="scss" scoped>
+  h3{
+    color:red;
+  }
+</style>
